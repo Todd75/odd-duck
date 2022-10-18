1e79057d4fa3d540fbe71e6ae0a3c900679ec149
Todd Wolden <tcwolden@gmail.com>
2022-10-17 22:44:28 -0600

    changes

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 'use strict';
 
+let myContainer = document.getElementById('displayImages');
+let resultBtn = document.getElementById('displayImages + resultsPick');
+let results = document.getElementById('resultsList');
