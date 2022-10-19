17d0fd244625272d5258e8b29f2e439540c3a10e
Todd Wolden <tcwolden@gmail.com>
2022-10-19 14:13:03 -0600

    Html updates

--- app.js
+++ app.js
@@ -68,97 +68,97 @@ function displayInventory() {
   inventoryArray[inventory3].views++;
   console.log(inventoryArray[inventory3].display);
 }
-// let indexArray = [];
+let indexArray = [];
 
-// function displayInventory() {
+function displayInventory() {
 
-//   while (indexArray.length < 6) {
-//     let ranNumber = randomInventory();
-//     if (!indexArray.includes(ranNumber)); {
-//       indexArray.push(ranNumber);
-//     }
-//   }
-//   console.log(indexArray);
-// }
-// let inventory1 = indexArray.shift();
-// let inventory2 = indexArray.shift();
-// let inventory3 = indexArray.shift();
+  while (indexArray.length < 6) {
+    let ranNumber = randomInventory();
+    if (!indexArray.includes(ranNumber)); {
+      indexArray.push(ranNumber);
+    }
+  }
+  console.log(indexArray);
+}
+let inventory1 = indexArray.shift();
+let inventory2 = indexArray.shift();
+let inventory3 = indexArray.shift();
 
-// const config = {
-//   type: 'bar',
-//   data: data,
-//   options: {
-//     scales: {
-//       y: {
-//         beginAtZero: true
-//       }
-//     }
-//   },
-// };
-// const labels = [];
-// let imageNames = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, waterCan, wineGlass];
-// let totalScore = [];
-// let totalViews = [];
-// for (let i = 0; i < inventoryArray.length; i++) {
-//   imageNames.push(inventoryArray[i].name);
-//   totalViews.push([i])
-// }
-// const data = {
-//   labels: labels,
-//   datasets: [{
-//     label: 'Number of Views',
-//     data: [65, 59, 80, 81, 56, 55, 40],
-//     backgroundColor: [
-//       'rgba(255, 99, 132, 0.2)',
-//       'rgba(255, 159, 64, 0.2)',
-//       'rgba(255, 205, 86, 0.2)',
-//       'rgba(75, 192, 192, 0.2)',
-//       'rgba(54, 162, 235, 0.2)',
-//       'rgba(153, 102, 255, 0.2)',
-//       'rgba(201, 203, 207, 0.2)'
-//     ],
-//     borderColor: [
-//       'rgb(255, 99, 132)',
-//       'rgb(255, 159, 64)',
-//       'rgb(255, 205, 86)',
-//       'rgb(75, 192, 192)',
-//       'rgb(54, 162, 235)',
-//       'rgb(153, 102, 255)',
-//       'rgb(201, 203, 207)'
-//     ],
-//     borderWidth: 1
-//   },
-//   {
-//     label: 'Number of Votes',
-//     data: [65, 59, 80, 81, 56, 55, 40],
-//     backgroundColor: [
-//       'rgba(255, 99, 132, 0.2)',
-//       'rgba(255, 159, 64, 0.2)',
-//       'rgba(255, 205, 86, 0.2)',
-//       'rgba(75, 192, 192, 0.2)',
-//       'rgba(54, 162, 235, 0.2)',
-//       'rgba(153, 102, 255, 0.2)',
-//       'rgba(201, 203, 207, 0.2)'
-//     ],
-//     borderColor: [
-//       'rgb(255, 99, 132)',
-//       'rgb(255, 159, 64)',
-//       'rgb(255, 205, 86)',
-//       'rgb(75, 192, 192)',
-//       'rgb(54, 162, 235)',
-//       'rgb(153, 102, 255)',
-//       'rgb(201, 203, 207)'
-//     ],
-//     borderWidth: 1
+const config = {
+  type: 'bar',
+  data: data,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  },
+};
 
-//   }
+let totalScore = [];
+let totalViews = [];
+for (let i = 0; i < inventoryArray.length; i++) {
+  imageNames.push(inventoryArray[i].name);
+  totalViews.push([i])
+}
+const labels = inventoryNames;
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'Number of Views',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
+    ],
+    borderWidth: 1
+  },
+  {
+    label: 'Number of Votes',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
+    ],
+    borderWidth: 1
 
-//   ]
-// };
-// const myChart = new Chart(
-//   document.getElementById('myChart'),
-//   config
-// );
+  }
+
+  ]
+};
+const myChart = new Chart(
+  document.getElementById('myChart'),
+  config
+);
 
 function displayResults() {
   for (let i = 0; i < inventoryArray.length; i++) {
@@ -196,4 +196,3 @@ function handleClicks(event) {
 myInventory.addEventListener('click', handleClicks);
 
 displayInventory();
-
