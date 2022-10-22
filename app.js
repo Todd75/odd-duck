6dd47eb48c0b583a26badb9bb5ba4c5544acddc1
Todd Wolden <tcwolden@gmail.com>
2022-10-22 15:44:20 -0600

    got help with an infinite loop I created

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
+
 // Global Variables
-let myInventory = document.querySelector('#oddDucksProject');
+let inventory = document.querySelector('#oddDucksProject');
 let image1 = document.querySelector('#imageOne');
 let image2 = document.getElementById('imageTwo');
 let image3 = document.querySelector('#imageThree');
@@ -10,12 +11,13 @@ let maxVotes = 25;
 let userVotes = 0;
 let indexArray = [];
 let inventoryArray = [];
-function MyInventory(name, fileExtension) {
+function MyInventory(src, name, fileExtension) {
   this.name = name;
   this.fileExtension = fileExtension;
   this.votes = 0;
   this.display = 0;
-  this.src = `img/${this.name}.${this.fileExtension}`;
+  this.src = src;
+  inventoryArray.push(this);
 }
 // Function creates the random images 
 function randomInventory() {
@@ -44,39 +46,35 @@ function displayInventory() {
   image3.alt = inventoryArray[inventory3].name;
   inventoryArray[inventory3].display++;
 }
+
 // pulls any data from local storage and creates an array of items
 function getData() {
-  let possibleData = localStorage.getItem('display');
-  if (possibleData) {
-    let dataDisplayed = JSON.parse(possibleData);
-    inventoryArray = dataDisplayed;
-    for (let item of dataDisplayed) {
-      let name = item.name;
-      let fileExtension = item.fileExtension;
-      let votes = item.votes;
-      let display = item.display;
-    }
+  let storedItems = localStorage.getItem('allProducts');
+  // load saved items from storage
+  if (storedItems) {
+    let parsedItem = JSON.parse(storedItems);
+    inventoryArray = parsedItem;
+    console.log(parsedItem);
   } else {
-    let bag = new MyInventory('bag', 'jpg');
-    let banana = new MyInventory('banana', 'jpg');
-    let bathroom = new MyInventory('bathroom', 'jpg');
-    let boots = new MyInventory('boots', 'jpg');
-    let breakfast = new MyInventory('breakfast', 'jpg');
-    let bubblegum = new MyInventory('bubblegum', 'jpg');
-    let chair = new MyInventory('chair', 'jpg');
-    let cthulhu = new MyInventory('cthulhu', 'jpg');
-    let dogDuck = new MyInventory('dog-duck', 'jpg');
-    let dragon = new MyInventory('dragon', 'jpg');
-    let pen = new MyInventory('pen', 'jpg');
-    let petSweep = new MyInventory('pet-sweep', 'jpg');
-    let scissors = new MyInventory('scissors', 'jpg');
-    let shark = new MyInventory('shark', 'jpg');
-    let sweep = new MyInventory('sweep', 'png');
-    let tauntaun = new MyInventory('tauntaun', 'jpg');
-    let unicorn = new MyInventory('unicorn', 'jpg');
-    let waterCan = new MyInventory('water-can', 'jpg');
-    let wineGlass = new MyInventory('wine-glass', 'jpg');
-    let inventoryArray = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, waterCan, wineGlass];
+    new MyInventory('img/bag.jpg', 'weird bag', 'bag');
+    new MyInventory('img/banana.jpg', 'weird banana', 'banana');
+    new MyInventory('img/bathroom.jpg', 'weird bathroom', 'bathroom');
+    new MyInventory('img/boots.jpg', 'weird boots', 'boots');
+    new MyInventory('img/breakfast.jpg', 'weird breakfast', 'breakfast');
+    new MyInventory('img/bubblegum.jpg', 'weird bubblegum', 'bubblegum');
+    new MyInventory('img/chair.jpg', 'weird chair', 'chair');
+    new MyInventory('img/cthulhu.jpg', 'weird cthulhu', 'cthulhu');
+    new MyInventory('img/dog-duck.jpg', 'weird dog duck', 'dog-duck');
+    new MyInventory('img/dragon.jpg', 'weird dragon', 'dragon');
+    new MyInventory('img/pen.jpg', 'weird pen', 'pen');
+    new MyInventory('img/pet-sweep.jpg', 'weird pet sweep', 'pet-sweep');
+    new MyInventory('img/scissors.jpg', 'weird scissors', 'scissors');
+    new MyInventory('img/shark.jpg', 'weird shark', 'shark');
+    new MyInventory('img/sweep.png', 'weird sweep', 'sweep');
+    new MyInventory('img/tauntaun.jpg', 'weird tauntaun', 'tauntaun');
+    new MyInventory('img/unicorn.jpg', 'weird unicorn', 'unicorn');
+    new MyInventory('img/water-can.jpg', 'weird water can', 'water-can');
+    new MyInventory('img/wine-glass.jpg', 'weird wine glass', 'wine-glass');
   }
 }
 // displays the results to the user
@@ -89,7 +87,7 @@ function displayResults() {
 }
 // click event that handles the user picking pictures
 function handleClicks(event) {
-  if (event.target === myInventory) {
+  if (event.target === inventory) {
     alert('Please select an image.');
   }
   userVotes++;
@@ -102,7 +100,7 @@ function handleClicks(event) {
     }
   }
   if (userVotes === maxVotes) {
-    myInventory.removeEventListener('click', handleClicks);
+    inventory.removeEventListener('click', handleClicks);
     displayResults();
     chartMaker();
   } else {
@@ -110,7 +108,7 @@ function handleClicks(event) {
     storedData();
   }
 }
-getData();
+// getData();
 // storing the data collected in local storage
 function storedData() {
   let viewedInventory = JSON.stringify(inventoryArray);
@@ -180,6 +178,7 @@ function chartMaker() {
     config
   );
 }
-myInventory.addEventListener('click', handleClicks);
+inventory.addEventListener('click', handleClicks);
 getData();
 displayInventory();
+
